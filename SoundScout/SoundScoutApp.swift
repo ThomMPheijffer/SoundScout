c40490342cf0d18d6f2e476b42d5efb12de1db01
Thom Pheijffer <tpheijffer@icloud.com>
2023-05-18 11:22:19 +0200

    Initial Commit

---
+++ SoundScout/SoundScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundScoutApp.swift
+//  SoundScout
+//
+//  Created by Thom Pheijffer on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SoundScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
