850604169d3388e6a0771a5560c20660948d8b77
Thom Pheijffer <tpheijffer@icloud.com>
2023-05-24 15:22:57 +0200

    Added Spotify class

--- SoundScout/SoundScoutApp.swift
+++ SoundScout/SoundScoutApp.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+import SpotifyWebAPI
 
 @main
 struct SoundScoutApp: App {
     @StateObject var navigationManager = NavigationManager.shared
+    @StateObject var spotify = Spotify()
+    
+    init() {
+        SpotifyAPILogHandler.bootstrap()
+    }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
 //            TeacherHomeScreen()
 //                .environmentObject(navigationManager)
+                .environmentObject(spotify)
         }
     }
 }
