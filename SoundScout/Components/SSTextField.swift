53e9208d52817de1085e65855c7f5807f9d912df
Thom Pheijffer <tpheijffer@icloud.com>
2023-06-03 13:01:46 +0200

    Fixed a bug where 2 textfields would appear instead of one

--- SoundScout/Components/SSTextField.swift
+++ SoundScout/Components/SSTextField.swift
@@ -27,24 +27,24 @@ struct SSTextField: View {
                         RoundedRectangle(cornerRadius: 20)
                             .stroke(SSColors.blue.opacity(0.3), lineWidth: 1)
                     )
-            }
-            
-            if axis == nil {
-                TextField(title, text: $text)
-                    .padding(.leading, 4)
-                    .padding(8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(SSColors.blue.opacity(0.3), lineWidth: 1)
-                    )
             } else {
-                TextField(title, text: $text, axis: axis!)
-                    .padding(.leading, 4)
-                    .padding(8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(SSColors.blue.opacity(0.3), lineWidth: 1)
-                    )
+                if axis == nil {
+                    TextField(title, text: $text)
+                        .padding(.leading, 4)
+                        .padding(8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(SSColors.blue.opacity(0.3), lineWidth: 1)
+                        )
+                } else {
+                    TextField(title, text: $text, axis: axis!)
+                        .padding(.leading, 4)
+                        .padding(8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(SSColors.blue.opacity(0.3), lineWidth: 1)
+                        )
+                }
             }
         }
     }
